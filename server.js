0e85e9bcc3ade381aa7d3fd1b6e4a472ad75b092
bvsswave <tannershahan@gmail.com>
2022-03-06 23:31:21 -0700

    Updated server.js

--- server.js
+++ server.js
@@ -16,9 +16,19 @@ app.use(express.static('public'));
 app.get('/api/notes',(req, res) => {
     res.json(allNotes);
 });
+
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
 });
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
 
 // creates new note
 
